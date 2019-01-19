97e166e63c2d3c4c50d2a6284bc953999f0e0622
Matt Erhart <mattjerhart@gmail.com>
2019-01-18 16:01:43 -0800

    kd tree, knn, search experiments

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ what's the expected outcome of some scenario (what's y given x)
 what scenario is optimal for creating some outcome (what predicts y)
 what does the author mean by x (use expands to & same as )
 how confident should I be in statement X
+
+# fast static kdtree: not sorted by distance?
+https://github.com/mourner/kdbush#readme
--- src/PageSvg.tsx
+++ src/PageSvg.tsx
@@ -3,7 +3,18 @@ import { Spring, animated } from "react-spring";
 import { dndContainer } from "./rx";
 import { Subscription } from "rxjs";
 import { TextItem } from "./PageText";
-// some example mouse coordinates
+var rbush = require("rbush");
+var knn = require("rbush-knn");
+import Flatbush from "flatbush";
+
+const title = {
+  height: 20,
+  width: 324,
+  x: 89,
+  x1: 89,
+  y: 53,
+  y1: 53
+};
 
 /**
  * @class **PageSvg**
@@ -12,7 +23,7 @@ import { TextItem } from "./PageText";
 const PageSvgDefaults = {
   props: { svgWidth: 0, svgHeight: 0, text: [] as TextItem[] },
   state: {
-    selectionRect: { x1: 0, y1: 0, x: 0, y: 0, width: 0, height: 0, show: true }
+    selectionRect: title
   }
 };
 export default class PageSvg extends React.Component<
@@ -24,6 +35,8 @@ export default class PageSvg extends React.Component<
   svgRef = React.createRef<SVGSVGElement>();
   sub: Subscription;
   componentDidMount() {
+    // this.getText(this.state.selectionRect);
+    this.snap(this.state.selectionRect);
     const dnd = dndContainer(this.svgRef);
     this.sub = dnd.subscribe(mouse => {
       const { selectionRect } = this.state;
@@ -76,6 +89,82 @@ export default class PageSvg extends React.Component<
     });
   }
 
+  // geom utils
+  getRectEdges = (x: number, y: number, width: number, height: number) => {
+    return {
+      minX: x,
+      minY: y,
+      maxX: x + width,
+      maxY: y + height
+    };
+  };
+
+  getRectCoords = (x: number, y: number, width: number, height: number) => {
+    //l = left, r = right, t = top, b = bottom
+    return {
+      lt: [x, y],
+      rt: [x + width, y],
+      lb: [x, y + height],
+      rb: [x + width, y + height]
+    };
+  };
+
+  min = (arr: number[]) => {
+    return arr.reduce(
+      (all, val, ix) => {
+        if (all.min < val) {
+          return all;
+        } else {
+          return { min: val, index: ix };
+        }
+      },
+      { min: Infinity, index: -1 }
+    );
+  };
+
+  snap = (selectionRect: typeof PageSvgDefaults.state.selectionRect) => {
+    const { x, y, width, height } = this.state.selectionRect;
+    const textCoords = this.props.text.map(t => {
+      const { left, top, width } = t;
+      const fontHeight = t.transform[0];
+      return this.getRectCoords(left, top, width, fontHeight);
+    });
+    const rectCoords = this.getRectCoords(x, y, width, height);
+
+    const ltDists = textCoords.map(t => {
+      const [x2, y2] = t.lt
+      const [x1, y1] = rectCoords.lt
+      return Math.hypot(x2 - x1, y2 - y1);
+    });
+    const ltNN = this.props.text[this.min(ltDists).index]
+     
+    
+    const rtDists = textCoords.map(t => {
+      const [x2, y2] = t.rt
+      const [x1, y1] = [rectCoords.rt[0], ltNN.top]
+      if (y2 > ltNN.top + ltNN.transform[0]) return Infinity 
+      return Math.hypot(x2 - x1, y2 - y1);
+    });
+    const rtNN = this.props.text[this.min(rtDists).index]
+    
+    const lbDists = textCoords.map(t => {
+      const [x2, y2] = t.rt
+      const [x1, y1] = [rectCoords.rt[0], ltNN.top]
+      if (y2 > ltNN.top + ltNN.transform[0]) return Infinity 
+      return Math.hypot(x2 - x1, y2 - y1);
+    });
+    const lbNN = this.props.text[this.min(rtDists).index]
+    
+
+    
+    // const concatStr = this.props.text
+    //   .slice(ids2[0])
+    //   .reduce((acc, val) => {
+    //     return acc + val.str.replace(/\s+/g, " ");
+    //   }, "");
+    //   console.log(this.props.text[ids2[0]])
+  };
+
   getText = (selectionRect: typeof PageSvgDefaults.state.selectionRect) => {
     const text = this.props.text.filter(tc => {
       const top = selectionRect.y;
@@ -89,10 +178,6 @@ export default class PageSvg extends React.Component<
       const xInRange = textX > left && textX < right;
       return yInRange && xInRange;
     });
-    const concatStr = text.reduce((acc, val) => {
-      return acc + val.str.replace(/\s+/g, " ");
-    }, "");
-    console.log(concatStr);
 
     //l 102 top 61
   };
@@ -103,7 +188,7 @@ export default class PageSvg extends React.Component<
 
   render() {
     const { x, y, width, height, show } = this.state.selectionRect;
-
+    console.log(this.state.selectionRect);
     return (
       <svg
         ref={this.svgRef}
@@ -111,6 +196,23 @@ export default class PageSvg extends React.Component<
         width={this.props.svgWidth}
         height={this.props.svgHeight}
       >
+        {this.props.text.map(t => {
+          return (
+            <rect
+              x={t.left}
+              y={t.top}
+              width={t.width}
+              height={t.transform[0]}
+              style={{
+                stroke: "black",
+                fill: "none",
+                strokeWidth: 1,
+                opacity: 0.5
+              }}
+            />
+          );
+        })}
+
         <Spring
           native
           to={{ x, y, width, height }}
