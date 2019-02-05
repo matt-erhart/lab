7503189cfbab80bea60c72336ad6e981ffc3e944
Matt Erhart <mattjerhart@gmail.com>
2019-02-04 16:28:30 -0800

    preproc pdfs as json files 10x faster loads

--- src/renderer/io.ts
+++ src/renderer/io.ts
@@ -3,8 +3,20 @@ import os = require("os");
 import path = require("path");
 import glob = require("glob");
 import uuidv1 = require("uuid/v1");
-const { homedir, username } = os.userInfo();
-const pdfDir = path.join(homedir, "pdfs");
+import jsonfile = require("jsonfile");
+import * as _pdfjs from "pdfjs-dist";
+var PdfjsWorker = require("pdfjs-dist/lib/pdf.worker.js");
+if (typeof window !== "undefined" && "Worker" in window) {
+  (_pdfjs as any).GlobalWorkerOptions.workerPort = new PdfjsWorker();
+}
+import {
+  PDFJSStatic,
+  PDFDocumentProxy,
+  PDFInfo,
+  PDFMetadata,
+  PDFTreeNode
+} from "pdfjs-dist";
+const pdfjs: PDFJSStatic = _pdfjs as any;
 
 interface FileInfo {
   pdfPath: string;
@@ -26,9 +38,23 @@ export const listPdfs = (fullPath: string): Promise<FileInfo[]> =>
     });
   });
 
-// export const mkdirForPdfs = ()
+export const listDirs = (fullpath: string): Promise<string[]> => {
+  return new Promise(resolve => {
+    glob(fullpath + "/*/", {}, (err, files) => {
+      resolve(files);
+    });
+  });
+};
 
-export const test = async () => {
+export const ls = (fullpath: string, options = {}): Promise<string[]> => {
+  return new Promise(resolve => {
+    glob(fullpath, options, (err, files) => {
+      resolve(files);
+    });
+  });
+};
+
+export const setupDirFromPdfs = async () => {
   const { homedir, username } = os.userInfo();
   const pdfDir = path.join(homedir, "pdfs");
   const infos = await listPdfs(pdfDir);
@@ -44,8 +70,54 @@ export const test = async () => {
     );
   }
 
-  glob(pdfDir + "/*/", {}, (err, files) => {
-    console.log(files);
-  });
+  const dirs = await listDirs(pdfDir);
+  await preprocessPdfs(dirs);
 };
-test();
+
+setupDirFromPdfs();
+
+const existsElseMake = async (path, promise) => {
+  const fileExists = await fs.pathExists(path);
+  if (!fileExists) {
+    const data = await promise;
+    await jsonfile.writeFile(path, data);
+  }
+};
+
+const preprocessPdfs = async (pdfDirs: string[]) => {
+  console.time("time");
+  const scale = 1;
+  
+
+  //   const dir = pdfDirs[0];
+  for (let dir of pdfDirs) {
+    const files = await ls(dir + "/*");
+    const [pdfPath] = files.filter(x => x.endsWith(".pdf"));
+    const pdf = await pdfjs.getDocument(pdfPath);
+    const allPageNumbers = [...Array(pdf.numPages).keys()].map(x => x + 1);
+
+    await existsElseMake(path.join(dir, "meta.json"), pdf.getMetadata());
+    await existsElseMake(path.join(dir, "outline.json"), pdf.getOutline());
+
+    for (const pageNumber of allPageNumbers) {
+      const outFile = path.join(dir, `text-page${pageNumber}.json`);
+      const fileExists = await fs.pathExists(outFile);
+      if (!fileExists) {
+        const page = await pdf.getPage(pageNumber);
+        const text = await page.getTextContent();
+        await jsonfile.writeFile(outFile, text);
+      }
+    }
+  }
+
+  console.timeEnd("time");
+};
+
+const loadPdfJson = async () => {
+  //   let loopState = [];
+  //   for (let jsonFile of files.filter(x => x.endsWith(".json"))) {
+  //     const next = jsonfile.readFile(jsonFile);
+  //     loopState.push(next);
+  //   }
+  //   const json = await Promise.all(loopState);
+};
