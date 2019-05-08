3955f1c707725a802647eacc5f98bf180cec1795
Xin Qian <xeniaqian94@gmail.com>
2019-05-08 13:27:57 -0400

    ctrl-4-view w/ color

--- src/renderer/SlateRichTextEditorNew.tsx
+++ src/renderer/SlateRichTextEditorNew.tsx
@@ -218,14 +218,14 @@ export class SlateRichTextEditor extends React.Component<any, any>{
     style: { verticalAlign: "middle" }
   };
 
-    /**
-   * On change, save the new `value`.
-   * (from Xin) This falls apart, however, because the user can usually type faster your server can respond.
-   *
-   * @param {Editor} editor
-   *
-   * mismatch between "editor" and "value" concepts  https://github.com/ianstormtaylor/slate/issues/2206
-   */
+  /**
+ * On change, save the new `value`.
+ * (from Xin) This falls apart, however, because the user can usually type faster your server can respond.
+ *
+ * @param {Editor} editor
+ *
+ * mismatch between "editor" and "value" concepts  https://github.com/ianstormtaylor/slate/issues/2206
+ */
 
   onChange = ({ value }) => {
     clearTimeout(this.timeout)
@@ -237,43 +237,216 @@ export class SlateRichTextEditor extends React.Component<any, any>{
     this.setState({ value })
   }
 
+  onKeyDown = (event, editor, next) => {
+    let mark
+
+    if (isBoldHotkey(event)) {
+      mark = 'bold'
+    } else if (isItalicHotkey(event)) {
+      mark = 'italic'
+    } else if (isUnderlinedHotkey(event)) {
+      mark = 'underlined'
+    } else if (isCodeHotkey(event)) {
+      mark = 'code'
+    } else if (isRecontextualize(event)) {
+      //   mark = 'recontextualize-hook'
+      console.log('mod+r clicked do insert the button')
+      this.editor.insertText('[x]')
+    } else {
+      return next()
+    }
+
+    event.preventDefault()
+    editor.toggleMark(mark)
+    // window.alert('onkeydown')
+  }
+
+  renderNode = (props, editor, next) => {
+    const { attributes, children, node } = props
+    switch (node.type) {
+      case 'block-quote':
+        return <blockquote {...attributes}>{children}</blockquote>
+      case 'bulleted-list':
+        return <ul {...attributes}>{children}</ul>
+      case 'heading-one':
+        return <h1 {...attributes}>{children}</h1>
+      case 'heading-two':
+        return <h2 {...attributes}>{children}</h2>
+      case 'list-item': // this is the node inside either bulleted-list or numbered-list
+        return <li {...attributes}>{children}</li>
+      case 'numbered-list':
+        return <ol {...attributes}>{children}</ol>
+      default:
+        return next()
+    }
+  }
+
+  /**
+ * Render a Slate mark.
+ *
+ *
+ * TODO: renderMark for recontext Hook
+ * @param {Object} props
+ * @return {Element}
+ */
+
+  renderMark = (props, editor, next) => {
+    const { children, mark, attributes } = props
+
+    switch (mark.type) {
+      case 'bold':
+        return (
+          <strong {...attributes}>
+            {children}
+          </strong>
+        )
+      case 'code':
+        return <code {...attributes}>{children}</code>
+      case 'italic':
+        return <em {...attributes}>{children}</em>
+      case 'underlined':
+        // console.log(editor.value.inlines.get(0))
+        return <u {...attributes}>{children}</u>
+      case 'recontextualize-hook':
+        return (
+          /* always use {children} https://github.com/ianstormtaylor/slate/issues/2142 */
+          <Button
+            //TODO: add hover function back!! 
+            // onMouseOver={() => {
+            //   const textBeforeHook = props.node.text.substring(0, props.offset)
+            //   this.props.onHoverHighlightCurrentUserInput(textBeforeHook)
+            // }}
+            // onMouseOut={() => {
+            //   const textBeforeHook = props.node.text.substring(0, props.offset)
+            //   this.props.offHoverHighlightCurrentUserInput(textBeforeHook)
+            // }}
+            // {...attributes} //attributes has few information
+            style={{ color: 'ForestGreen' }}
+          >
+            <b>{children}</b>
+          </Button>
+        )
+      case 'mechanism-span':
+        return <b style={{ color: 'SteelBlue' }}>{children}</b> // color codes https://www.quackit.com/css/css_color_codes.cfm
+      case 'purpose-span':
+        return <b style={{ color: 'Salmon' }}>{children}</b>
+      default:
+        return next()
+    }
+  }
+
+  decorateNode = (node, editor, next) => {
+    const others = next() || []
+    let decorations = []
+    let startText = node.getFirstText()
+
+    let re = /\[x\]/g
+    re.lastIndex = 0
+    // The lastIndex is a read/write integer property of regular expression 
+    // instances that specifies the index at which to start the next match.
+    let str = startText.text
+    var match
+
+    while ((match = re.exec(str)) !== null) {
+      const dec = {
+        anchor: {
+          key: startText.key,
+          offset: match.index,
+        },
+        focus: {
+          key: startText.key,
+          offset: match.index + 3,
+        },
+        mark: {
+          type: 'recontextualize-hook',
+        },
+      }
+      decorations.push(dec)
+    }
+
+    re = /[Ww]ith/g
+    re.lastIndex = 0
+    str = startText.text
+
+    if ((match = re.exec(str)) !== null) {
+      const dec = {
+        anchor: {
+          key: startText.key,
+          offset: match.index,
+        },
+        focus: {
+          key: startText.key,
+          offset: str.length - 1// match.index + 10,
+        },
+        mark: {
+          type: 'mechanism-span',
+        },
+      }
+      decorations.push(dec)
+    }
+
+    re = /[Tt]o /g
+    re.lastIndex = 0
+    str = startText.text
+
+    if ((match = re.exec(str)) !== null) {
+      const dec = {
+        anchor: {
+          key: startText.key,
+          offset: match.index,
+        },
+        focus: {
+          key: startText.key,
+          offset: str.length// match.index + 10,
+        },
+        mark: {
+          type: 'purpose-span',
+        },
+      }
+      decorations.push(dec)
+    }
+
+    // editor.deselect().focus() //Or other commands??
+    return [...others, ...decorations]
+  }
+
   render() {
     return (
       <div>
-      <Toolbar>
-        {this.renderMarkButton('bold' as SlateTypes, <MdFormatBold {...this.iconProps} />)}
-        {this.renderMarkButton('italic' as SlateTypes, <MdFormatItalic {...this.iconProps} />)}
-        {this.renderMarkButton('underlined' as SlateTypes, <MdFormatUnderlined {...this.iconProps} />)}
-        {this.renderMarkButton('code' as SlateTypes, <MdCode {...this.iconProps} />)}
-        {this.renderBlockButton('heading-one' as SlateTypes, <MdLooksOne {...this.iconProps} />)}
-        {this.renderBlockButton('heading-two' as SlateTypes, <MdLooksTwo {...this.iconProps} />)}
-        {this.renderBlockButton('block-quote' as SlateTypes, <MdFormatQuote {...this.iconProps} />)}
-        {this.renderBlockButton('numbered-list' as SlateTypes, <MdFormatListNumbered {...this.iconProps} />)}
-        {this.renderBlockButton('bulleted-list' as SlateTypes, <MdFormatListBulleted {...this.iconProps} />)}
-        {this.renderMarkButton(
-          'recontextualize_hook' as SlateTypes, <MdFindInPage {...this.iconProps}><div>cmd+r</div></MdFindInPage>)}
+        <Toolbar>
+          {this.renderMarkButton('bold' as SlateTypes, <MdFormatBold {...this.iconProps} />)}
+          {this.renderMarkButton('italic' as SlateTypes, <MdFormatItalic {...this.iconProps} />)}
+          {this.renderMarkButton('underlined' as SlateTypes, <MdFormatUnderlined {...this.iconProps} />)}
+          {this.renderMarkButton('code' as SlateTypes, <MdCode {...this.iconProps} />)}
+          {this.renderBlockButton('heading-one' as SlateTypes, <MdLooksOne {...this.iconProps} />)}
+          {this.renderBlockButton('heading-two' as SlateTypes, <MdLooksTwo {...this.iconProps} />)}
+          {this.renderBlockButton('block-quote' as SlateTypes, <MdFormatQuote {...this.iconProps} />)}
+          {this.renderBlockButton('numbered-list' as SlateTypes, <MdFormatListNumbered {...this.iconProps} />)}
+          {this.renderBlockButton('bulleted-list' as SlateTypes, <MdFormatListBulleted {...this.iconProps} />)}
+          {this.renderMarkButton(
+            'recontextualize_hook' as SlateTypes, <MdFindInPage {...this.iconProps}><div>cmd+r</div></MdFindInPage>)}
 
-        {/* TODO when user click this button, it will insert into current cursor position a reconteztualize hook, e.g. [cite!] or [x] */}
-        {/* <LinkButton /> */}
-      </Toolbar>
+          {/* TODO when user click this button, it will insert into current cursor position a reconteztualize hook, e.g. [cite!] or [x] */}
+          {/* <LinkButton /> */}
+        </Toolbar>
 
-      <Editor
-        spellCheck
-        autoFocus
-        placeholder="Enter some rich text..."
-        ref={this.ref as any}
-        // ref={this.refRealEditor as any}
-        value={this.state.value}
-        onChange={this.onChange}
-        // onKeyDown={this.onKeyDown}
-        // renderNode={this.renderNode}
-        // renderMark={this.renderMark}
-        // onMouseDown={this.onMouseDown}
-        // onMouseUp={this.onMouseUp}
-        // decorateNode={this.decorateNode}
+        <Editor
+          spellCheck
+          autoFocus
+          placeholder="Enter some rich text..."
+          ref={this.ref as any}
+          // ref={this.refRealEditor as any}
+          value={this.state.value}
+          onChange={this.onChange}
+          onKeyDown={this.onKeyDown}
+          renderNode={this.renderNode}
+          renderMark={this.renderMark}
+          // onMouseDown={this.onMouseDown}
+          // onMouseUp={this.onMouseUp}
+          decorateNode={this.decorateNode}
         // onClick={this.onClick}
-      // plugins={plugins}
-      />
+        // plugins={plugins}
+        />
       </div>
 
     )
